ca77d65534d116e5e081e7533ff527f36abbfc85
ElliotKoch <kochelliotpro@gmail.com>
2025-03-04 15:23:30 +0100

    Add handler

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import discord
+import json
+import requests
+from dotenv import load_dotenv
+import os
+import logging
 
+# Keys are stored locally in the '.env' file for security reasons.
+load_dotenv()
 intents = discord.Intents.default()
 intents.message_content = True
-
 client = discord.Client(intents=intents)
+
+github_data = json.load(open("data.json"))
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
 
 @client.event
 async def on_ready():
@@ -11,12 +21,14 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    print(message.author, client.user)
+    
     if message.author == client.user:
         return
 
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
 
-client.run('MTM0NDk3MTM2ODgyMjczODk0NA.GZqbWk.NsPci512kJWQrVtndK6_e-aTSOSsfsYybUZALY')
 
+# Creates a handler for the logs
+#handler = logging.FileHandler(filename='logs/discord.log', encoding='utf-8', mode='w')
+client.run(BOT_TOKEN)#, log_handler=handler, log_level=logging.DEBUG)
